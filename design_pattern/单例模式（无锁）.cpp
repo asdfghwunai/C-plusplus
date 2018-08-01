3123b15117d83c3ac44f7b444c143c2b8102dce4
asdfghwunai <1729589198@qq.com>
2018-08-01 19:37:58 +0800

    Update 单例模式（无锁）.cpp

--- design_pattern/单例模式（无锁）.cpp
+++ design_pattern/单例模式（无锁）.cpp
@@ -32,3 +32,38 @@ int main()
 
 	return 0;
 }
+
+//=====================================================
+#include<iostream>
+using namespace std;
+
+class Singlelon
+{
+
+private:
+	static Singlelon *p;
+	Singlelon(){ cout << "构造函数"<<endl; }
+	Singlelon(const Singlelon& m){}
+	Singlelon& operator=(const Singlelon& m);
+public:
+	static Singlelon* instance();
+};
+
+Singlelon* Singlelon::instance()
+{
+	
+	return p;
+}
+
+Singlelon * Singlelon::p = new Singlelon();   //为啥可以访问,写到这里也不是函数吧
+
+int main()
+{
+	Singlelon* s1=Singlelon::instance();
+
+	Singlelon* s2 = Singlelon::instance();
+
+	while (1);
+
+	return 0;
+}
