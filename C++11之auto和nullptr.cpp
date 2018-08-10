2099c9475ab6b7bc14281ed132a154db10f4e155
asdfghwunai <1729589198@qq.com>
2018-08-10 15:25:38 +0800

    Create C++11之auto和nullptr.cpp

---
+++ C++11之auto和nullptr.cpp
@@ -0,0 +1,47 @@
+/*
+auto可以自动推导类型，但是不能用auto来声明函数，但是和定义函数放一起就可以声明
+*/
+
+//示例代码1.0 http://www.cnblogs.com/feng-sc/p/5710724.html
+auto AddTest(int a, int b)    //声明加定义
+{
+    return a + b;
+}
+
+int main()
+{
+    auto index = 10;
+    auto str = "abc";
+    auto ret = AddTest(1,2);
+    std::cout << "index:" << index << std::endl;
+    std::cout << "str:" << str << std::endl;
+    std::cout << "res:" << ret << std::endl;
+}
+
+
+//=====================================
+
+/*
+NULL不安全，会被当成整数0
+*/
+
+//示例代码1.0 http://www.cnblogs.com/feng-sc/p/5710724.html
+class Test
+{
+public:
+    void TestWork(int index)
+    {
+        std::cout << "TestWork 1" << std::endl;
+    }
+    void TestWork(int * index)
+    {
+        std::cout << "TestWork 2" << std::endl;
+    }
+};
+
+int main()
+{
+    Test test;
+    test.TestWork(NULL);
+    test.TestWork(nullptr);
+}
