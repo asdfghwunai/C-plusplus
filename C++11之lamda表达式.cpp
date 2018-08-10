7ff8280661e0e47fa45e8f46550c9faf81fbddc5
asdfghwunai <1729589198@qq.com>
2018-08-10 16:33:23 +0800

    Create C++11之lamda表达式.cpp

---
+++ C++11之lamda表达式.cpp
@@ -0,0 +1,33 @@
+//示例代码1.0 http://www.cnblogs.com/feng-sc/p/5710724.html
+int main()
+{
+    auto add= [](int a, int b)->int{
+        return a + b;
+    };
+    int ret = add(1,2);
+    std::cout << "ret:" << ret << std::endl;
+    return 0;
+}
+
+/*
+普通的，
+lamda表达式是一个匿名函数，居然可以写在式子里，后面有；
+*/
+============================
+int main()
+{
+    Test test;
+    test.Add(add, 1, 2);
+
+    TestAdd testAdd;
+    test.Add(std::bind(&TestAdd::Add, testAdd, std::placeholders::_1, std::placeholders::_2), 1, 2);
+
+    test.Add([](int a, int b)->int {
+        std::cout << "lamda add fun" << std::endl;
+        return a + b;
+    },1,2);
+    return 0;
+}
+/*
+函数对象
+*/
